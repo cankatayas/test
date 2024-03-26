c6934cacd039a2f1fd7e93ec7d7036556959fb2e
cankatayas <64742047+cankatayas@users.noreply.github.com>
2024-03-26 13:04:34 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+
+
+const hamburger=document.getElementById('hamburger');
+const navBAR=document.getElementById('navul-h');
+
+
+hamburger.addEventListener('click', () => {
+    navBAR.classList.toggle('show');
+});
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+
+
+
+
